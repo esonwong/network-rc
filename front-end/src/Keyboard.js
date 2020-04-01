785bddcf7e8dc81d72b53d5c77d64ae500ce6251
Eson Wong <itiwll@gmail.com>
2020-04-01 20:44:06 +0800

    Update

---
+++ front-end/src/Keyboard.js
@@ -0,0 +1,71 @@
+import React, { Component, createRef } from "react";
+
+export default class Keybord extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = createRef();
+  }
+
+  componentDidMount() {
+    this.keyboardBind();
+  }
+
+  keyboardBind = () => {
+    const {
+      props: {
+        controller: { speed, direction }
+      }
+    } = this;
+    this.ref.current.addEventListener(
+      "keydown",
+      event => {
+        const keyName = event.key;
+        if (keyName === "w") {
+          speed(1);
+        }
+        if (keyName === "s") {
+          speed(-1);
+        }
+        if (keyName === "a") {
+          direction(1);
+        }
+        if (keyName === "d") {
+          direction(-1);
+        }
+      },
+      false
+    );
+    this.ref.current.addEventListener(
+      "keyup",
+      event => {
+        const {
+          props: {
+            controller: { speed, direction }
+          }
+        } = this;
+        const keyName = event.key;
+        if (keyName === "w") {
+          speed(0);
+        }
+        if (keyName === "s") {
+          speed(0);
+        }
+        if (keyName === "a") {
+          direction(0);
+        }
+        if (keyName === "d") {
+          direction(0);
+        }
+      },
+      false
+    );
+  };
+
+  render() {
+    return (
+      <div  tabIndex="0" ref={this.ref}>
+        wads
+      </div>
+    );
+  }
+}
