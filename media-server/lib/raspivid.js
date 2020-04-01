4ab4f537cfad243e3a378f082e51ea4a9caf4498
Eson Wong <itiwll@gmail.com>
2020-04-01 11:13:28 +0800

    Update

---
+++ media-server/lib/raspivid.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const util = require("util");
+const spawn = require("child_process").spawn;
+const merge = require("mout/object/merge");
+
+const Server = require("./_server");
+
+class RpiServer extends Server {
+  constructor(server, opts) {
+    super(
+      server,
+      merge(
+        {
+          fps: 12
+        },
+        opts
+      )
+    );
+  }
+
+  get_feed() {
+    var msk = "raspivid -t 0 -o - -w %d -h %d -fps %d";
+    var cmd = util.format(
+      msk,
+      this.options.width,
+      this.options.height,
+      this.options.fps
+    );
+    console.log(cmd);
+    var streamer = spawn("raspivid", [
+      "-t",
+      "0",
+      "-o",
+      "-",
+      "-w",
+      this.options.width,
+      "-h",
+      this.options.height,
+      "-fps",
+      this.options.fps,
+      "-pf",
+      "baseline"
+    ]);
+    streamer.on("exit", function(code) {
+      console.log("Failure", code);
+    });
+
+    this.streamer = streamer;
+
+    return streamer.stdout;
+  }
+
+  stop() {
+    this.streamer.kill();
+  }
+}
+
+module.exports = RpiServer;
