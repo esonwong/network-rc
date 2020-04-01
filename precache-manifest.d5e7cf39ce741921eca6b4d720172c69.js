6f6409b3369f7324bd18db108bd67dff9e39f38e
itiwll <itiwll@users.noreply.github.com>
2020-04-01 01:04:44 +0000

    deploy: e40e9b85a70a258137f5731ecec58c9cb2a72f46

---
+++ precache-manifest.d5e7cf39ce741921eca6b4d720172c69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4eb5c91548ecb7bbba0b8c10820ee49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7353d7f7968c74bf8f8",
+    "url": "/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "389ccaddb343150f18f4",
+    "url": "/static/css/main.af890be8.chunk.css"
+  },
+  {
+    "revision": "f7353d7f7968c74bf8f8",
+    "url": "/static/js/2.b44ba986.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b44ba986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389ccaddb343150f18f4",
+    "url": "/static/js/main.11cb0de5.chunk.js"
+  },
+  {
+    "revision": "f26ba8da7c41e270b6e8",
+    "url": "/static/js/runtime-main.c4f1d106.js"
+  }
+]);
