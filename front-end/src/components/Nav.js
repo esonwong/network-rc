2cfb3953ae9bae39d381517c31e74bbaaef5047c
Eson Wong <itiwll@gmail.com>
2020-04-06 13:55:05 +0800

    Update the controller

---
+++ front-end/src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Menu } from "antd";
+import { Link } from "@reach/router";
+
+export default function Nav(props) {
+  return (
+    <Menu mode="horizontal" {...props}>
+      <Menu.Item>
+        <Link to="controller">控制</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="setting">设置</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="ai">Ai</Link>
+      </Menu.Item>
+    </Menu>
+  );
+}
