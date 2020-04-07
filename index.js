6ac55099a72b93545bac186a45871bb70d10d901
Eson Wong <itiwll@gmail.com>
2020-04-07 12:48:26 +0800

    Update the server

--- index.js
+++ index.js
@@ -6,11 +6,41 @@ const app = express();
 const server = require("http").Server(app);
 const spawn = require("child_process").spawn;
 const { WebSocketServer } = require("@clusterws/cws");
+const rpio = require("rpio");
 
 app.use(express.static(path.resolve(__dirname, "./front-end/build")));
 
 const width = 400,
   height = 300;
+
+const directionPin = 32,
+  speedPin = 33;
+rpio.open(directionPin, rpio.PWM);
+rpio.open(speedPin, rpio.PWM);
+rpio.pwmSetClockDivider(512);
+rpio.pwmSetRange(directionPin, 100);
+rpio.pwmSetRange(speedPin, 100);
+rpio.pwmSetData(speedPin, 55);
+rpio.pwmSetData(directionPin, 60);
+
+// direction range 100 data 30 ~ 90
+// speed range 100 data 36~60~80
+
+const changeSpeed = function (v) {
+  if (v == 0) {
+    rpio.pwmSetData(speedPin, 0);
+  } else {
+    rpio.pwmSetData(speedPin, 60 + v * 20);
+  }
+};
+
+const changeDirection = function (v) {
+  if (v == 0) {
+    rpio.pwmSetData(directionPin, 0);
+  } else {
+    rpio.pwmSetData(directionPin, 60 + v * 30);
+  }
+};
 
 const wss = new WebSocketServer({ server });
 const avcServer = new AvcServer(wss, width, height);
@@ -30,12 +60,14 @@ avcServer.client_events.on("speed zero rate", (rate) => {
   console.log("speed zero rate", rate);
 });
 
-avcServer.client_events.on("speed rate", (rate) => {
-  console.log("speed rate", rate);
+avcServer.client_events.on("speed rate", (v) => {
+  console.log("speed", v);
+  changeSpeed(v);
 });
 
-avcServer.client_events.on("direction rate", (rate) => {
-  console.log("direction rate", rate);
+avcServer.client_events.on("direction rate", (v) => {
+  console.log("direction", v);
+  changeDirection(v);
 });
 
 avcServer.on("client_disconnected", () => {
@@ -48,29 +80,28 @@ avcServer.on("client_disconnected", () => {
   }
 });
 
-
 let streamer = null;
 
 const startStreamer = () => {
   console.log("starting streamer");
   streamer = spawn("raspivid", [
-     "-pf",
-     "baseline",
-     "-ih",
-     "-t",
-     "0",
-     "-w",
-     width,
-     "-h",
-     height,
-     "-hf",
-     "-fps",
-     "15",
-     "-g",
-     "30",
-     "-o",
-     "-",
-   ]);
+    "-pf",
+    "baseline",
+    "-ih",
+    "-t",
+    "0",
+    "-w",
+    width,
+    "-h",
+    height,
+    "-hf",
+    "-fps",
+    "15",
+    "-g",
+    "30",
+    "-o",
+    "-",
+  ]);
   streamer.on("close", () => {
     streamer = null;
   });
