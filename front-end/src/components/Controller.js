4bdef89f2a96365b5d8b9fb74dc23b8843bef068
Eson Wong <itiwll@gmail.com>
2020-04-12 19:47:09 +0800

    Update the UI

--- front-end/src/components/Controller.js
+++ front-end/src/components/Controller.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Form, Button, Switch, Slider } from "antd";
+import { Form, Button, Switch, Slider, Popover } from "antd";
+import { SlidersOutlined } from "@ant-design/icons";
 import {
   AimOutlined,
   DownOutlined,
@@ -31,6 +32,7 @@ export default class Controller extends Component {
       speedReverse: store.get("speedReverse") || false,
       backwardPower: 50,
       forwardPower: 50,
+      directionFix: store.get("directionFix") || 0,
     };
   }
 
@@ -58,52 +60,103 @@ export default class Controller extends Component {
     controller.direction((bateDegree / 30) * (directionReverse ? -1 : 1));
   };
 
+  fixedController = {
+    speed: (v) => {
+      const {
+        props: {
+          controller: { speed },
+        },
+        state: { forwardPower, backwardPower, speedReverse },
+      } = this;
+      speed(
+        v *
+          (speedReverse ? -1 : 1) *
+          ((v > 0 ? forwardPower : backwardPower) / 100)
+      );
+    },
+    direction: (v) => {
+      const {
+        props: {
+          controller: { direction },
+        },
+        state: { directionReverse, directionFix },
+      } = this;
+      direction(v * (directionReverse ? -1 : 1) + directionFix);
+    },
+  };
+
+  fixContent = () => {
+    const { directionReverse, speedReverse, directionFix } = this.state;
+    return (
+      <Form>
+        <Form.Item
+          style={{ display: isSupportedOrientaion ? undefined : "none" }}
+        >
+          <Button
+            type="danger"
+            onClick={() => {
+              this.setState({ zeroOrientation: { ...curentOrientation } });
+            }}
+            icon={<AimOutlined />}
+          >
+            舵机感应校准
+          </Button>
+        </Form.Item>
+        <Form.Item label="舵机反向">
+          <Switch
+            checked={directionReverse}
+            onChange={(v) => {
+              this.setState({
+                directionReverse: v,
+              });
+              store.set("directionReverse", v);
+            }}
+          />
+        </Form.Item>
+        <Form.Item label="油门反向">
+          <Switch
+            checked={speedReverse}
+            onChange={(v) => {
+              this.setState({
+                speedReverse: v,
+              });
+              store.set("speedReverse", v);
+            }}
+          />
+        </Form.Item>
+        <Form.Item label="舵机微调">
+          <Slider
+            value={directionFix * 50 + 50}
+            min={0}
+            max={100}
+            included={false}
+            onChange={(v) => {
+              store.set("directionFix", v / 100);
+              const directionFix = v / 50 - 1;
+              this.setState({ directionFix }, () => {
+                this.fixedController.direction(0);
+              });
+            }}
+            style={{ width: "50vw" }}
+          />
+        </Form.Item>
+      </Form>
+    );
+  };
+
   render() {
-    const { controller } = this.props;
     const {
-      directionReverse,
-      forwardPower,
-      backwardPower,
-      speedReverse,
-    } = this.state;
+      fixContent,
+      fixedController: { speed, direction },
+    } = this;
+    const { forwardPower, backwardPower } = this.state;
     return (
       <div className="controller">
         <Form className="controller-form" size="small" layout="inline">
-          <Form.Item
-            style={{ display: isSupportedOrientaion ? undefined : "none" }}
-          >
-            <Button
-              type="danger"
-              onClick={() => {
-                this.setState({ zeroOrientation: { ...curentOrientation } });
-              }}
-              icon={<AimOutlined />}
-            >
-              舵机校准
-            </Button>
-          </Form.Item>
-          <Form.Item label="舵机反向">
-            <Switch
-              checked={directionReverse}
-              onChange={(v) => {
-                this.setState({
-                  directionReverse: v,
-                });
-                store.set("directionReverse", v);
-              }}
-            />
-          </Form.Item>
-          <Form.Item label="油门反向">
-            <Switch
-              checked={speedReverse}
-              onChange={(v) => {
-                debugger;
-                this.setState({
-                  speedReverse: v,
-                });
-                store.set("speedReverse", v);
-              }}
-            />
+          <Form.Item>
+            <Popover content={fixContent} title="修正" trigger="click">
+              <Button icon={<SlidersOutlined />}>修正</Button>
+            </Popover>
           </Form.Item>
           <Button
             className="left-button"
@@ -111,25 +164,24 @@ export default class Controller extends Component {
             size="large"
             type="primary"
             onTouchStart={() => {
-              controller.direction(directionReverse ? -1 : 1);
+              direction(1);
             }}
             onTouchEnd={() => {
-              controller.direction(0);
+              direction(0);
             }}
             icon={<LeftOutlined />}
             style={{ display: !isSupportedOrientaion ? undefined : "none" }}
           ></Button>
-
           <Button
             className="right-button"
             shape="circle"
             size="large"
             type="primary"
             onTouchStart={() => {
-              controller.direction(directionReverse ? 1 : -1);
+              direction(-1);
             }}
             onTouchEnd={() => {
-              controller.direction(0);
+              direction(0);
             }}
             icon={<RightOutlined />}
             style={{ display: !isSupportedOrientaion ? undefined : "none" }}
@@ -140,32 +192,28 @@ export default class Controller extends Component {
             size="large"
             type="primary"
             onTouchStart={() => {
-              controller.speed(
-                ((speedReverse ? -1 : 1) * (1 * forwardPower)) / 100
-              );
+              speed(1);
             }}
             onTouchEnd={() => {
-              controller.speed(0);
+              speed(0);
             }}
             icon={<UpOutlined />}
           ></Button>
-
           <Button
             className="backward-button"
             shape="circle"
             size="large"
             type="primary"
             onTouchStart={() => {
-              controller.speed(
-                ((speedReverse ? -1 : 1) * (-1 * backwardPower)) / 100
-              );
+              speed(-1);
             }}
             onTouchEnd={() => {
-              controller.speed(0);
+              speed(0);
             }}
             icon={<DownOutlined />}
           ></Button>
 
+          <Form.Item label="键盘">wsad</Form.Item>
           <Form.Item label="前进油门">
             <Slider
               value={forwardPower}
@@ -177,7 +225,6 @@ export default class Controller extends Component {
               style={{ width: "15vw" }}
             />
           </Form.Item>
-
           <Form.Item label="倒退油门">
             <Slider
               value={backwardPower}
@@ -190,13 +237,7 @@ export default class Controller extends Component {
             />
           </Form.Item>
         </Form>
-        <Keybord
-          controller={controller}
-          backwardPower={backwardPower}
-          forwardPower={forwardPower}
-          directionReverse={directionReverse}
-          speedReverse={speedReverse}
-        />
+        <Keybord controller={{ speed, direction }} />
       </div>
     );
   }
