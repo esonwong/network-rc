e832f4bfdb5867969fc99f2fd650bd6189a42dec
Eson Wong <itiwll@gmail.com>
2020-04-20 19:09:25 +0800

    Update the gamepad feature

---
+++ front-end/build/precache-manifest.796a73ceab4cbddee6870f1bbc323a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba7acdbad18f3aefb7002d0ea596072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0edaac0bdb033a17e54",
+    "url": "/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "2f7e8060fc8b7df0ecb0",
+    "url": "/static/css/main.58750cd4.chunk.css"
+  },
+  {
+    "revision": "f0edaac0bdb033a17e54",
+    "url": "/static/js/2.51a1af45.chunk.js"
+  },
+  {
+    "revision": "f72996d9aae03e2edd2761b696e83056",
+    "url": "/static/js/2.51a1af45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7e8060fc8b7df0ecb0",
+    "url": "/static/js/main.88929247.chunk.js"
+  },
+  {
+    "revision": "f26ba8da7c41e270b6e8",
+    "url": "/static/js/runtime-main.c4f1d106.js"
+  }
+]);
