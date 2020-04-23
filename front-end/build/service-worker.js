95aade9fd8c1ca7ec463823a6a3dda12d3127c0e
Eson Wong <itiwll@gmail.com>
2020-04-23 22:34:11 +0800

    Update UI

--- front-end/build/service-worker.js
+++ front-end/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8d0153039258a5b99f8b6ab6fd7ed65.js"
+  "/precache-manifest.013b6d2be510f5034355d061e2282d7e.js"
 );
 
 self.addEventListener('message', (event) => {
