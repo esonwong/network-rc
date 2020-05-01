8e229cb344bc7987a150c5b15e89a02c413bb90c
Eson Wong <itiwll@gmail.com>
2020-05-01 11:48:11 +0800

    Update object detection

---
+++ front-end/build/precache-manifest.ac8af985ca704f19072bb502cfbc4127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09545bb5b010eb6a563e3e05d9ce0739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ede248c4c7149a49770a",
+    "url": "/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "c43751c460690778c3ce",
+    "url": "/static/css/main.abb41682.chunk.css"
+  },
+  {
+    "revision": "ede248c4c7149a49770a",
+    "url": "/static/js/2.4d9c348c.chunk.js"
+  },
+  {
+    "revision": "7c3d977ff463816950b5e8b459ee8542",
+    "url": "/static/js/2.4d9c348c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43751c460690778c3ce",
+    "url": "/static/js/main.af4e74ab.chunk.js"
+  },
+  {
+    "revision": "f26ba8da7c41e270b6e8",
+    "url": "/static/js/runtime-main.c4f1d106.js"
+  }
+]);
