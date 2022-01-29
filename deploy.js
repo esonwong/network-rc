50e111742e76c736f66ff30cd4cd5b6d828e47f9
Eson Wong <esonzero@gmail.com>
2022-01-29 07:47:14 +0000

    Chore: optimize install & deploy scripts

--- deploy.js
+++ deploy.js
@@ -2,16 +2,30 @@ const util = require("util");
 const pkg = require("./package.json");
 const exec = util.promisify(require("child_process").exec);
 
+const argv = require("yargs").options({
+  b: {
+    alias: "beta",
+    describe: "密码",
+    type: "boolean",
+    default: false,
+  },
+}).argv;
+
+const beta = argv.beta;
+
+const fullFileName = `${pkg.name}-${pkg.version}${beta ? "-beta" : ""}.tar.gz`;
+const fileName = `${pkg.name}${beta ? "-beta" : ""}.tar.gz`;
+
 (async function () {
   try {
     console.log("上传到 nas");
 
     // await exec(
-    //   `scp -P 5022 dist/network-rc.tar.gz itiwll@192.168.50.2:/volume1/web/network-rc/download/network-rc.tar.gz`
+    //   `scp -P 5022 dist/network-rc.tar.gz itiwll@192.168.50.2:/volume1/web/network-rc/download/${fileName}`
     // );
 
     // await exec(
-    //   `scp -P 5022 dist/network-rc.tar.gz itiwll@192.168.50.2:/volume1/web/network-rc/download/network-rc-v${pkg.version}.tar.gz`
+    //   `scp -P 5022 dist/network-rc.tar.gz itiwll@192.168.50.2:/volume1/web/network-rc/download/${fullFileName}`
     // );
 
     console.log("上传 一键安装脚本");
@@ -20,18 +34,16 @@ const exec = util.promisify(require("child_process").exec);
       `scp -P 29168 ./install.sh root@download.esonwong.com:/www/download/network-rc/install.sh`
     );
 
-    console.log("上传 network-rc.tar.gz 到 download.esonwong.com");
+    console.log(`上传 ${fileName} 到 download.esonwong.com`);
 
     await exec(
-      `scp -P 29168 dist/network-rc.tar.gz root@download.esonwong.com:/www/download/network-rc/network-rc.tar.gz`
+      `scp -P 29168 dist/network-rc.tar.gz root@download.esonwong.com:/www/download/network-rc/${fileName}`
     );
 
-    console.log(
-      `上传 network-rc-v${pkg.version}.tar.gz 到 download.esonwong.com`
-    );
+    console.log(`上传 ${fullFileName} 到 download.esonwong.com`);
 
     await exec(
-      `scp -P 29168 dist/network-rc.tar.gz root@download.esonwong.com:/www/download/network-rc/network-rc-v${pkg.version}.tar.gz`
+      `scp -P 29168 dist/network-rc.tar.gz root@download.esonwong.com:/www/download/network-rc/${fullFileName}`
     );
   } catch (e) {
     console.error(e);
