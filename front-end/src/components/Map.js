17e769a934e70de0829bd15a0b2296d420f5f5e6
Eson Wong <itiwll@gmail.com>
2022-03-05 12:59:44 +0800

    Feature: add gps path

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -1,43 +1,56 @@
 import React, { useEffect, useState } from "react";
-import "./Map.scss";
-import { Amap, Marker, config } from "@amap/amap-react";
+import { Amap, Marker, config, Polyline } from "@amap/amap-react";
 import store from "store";
+import { Switch } from "antd";
+import styles from "./Map.module.scss";
 
 config.key = "8faf092bfa96e5b6748ea7e0a2d6ac9c";
 
-export default function Map({
-  statusInfo: { gps = { lng: 116.478935, lat: 39.997761 } } = {},
-}) {
-  const { lat, lng } = gps;
+export default function Map({ statusInfo: { gps = {} } = {} }) {
+  const { lat = 39, lng = 116 } = gps;
   const center = [lng, lat];
-  const [history, setHistory] = useState(store.get("gps history") || []);
+
+  const [history, setHistory] = useState(
+    (store.get("gps history") || [])
+      .filter(({ lat, lng }) => lat !== undefined && lng !== undefined)
+      .slice(0, 1000)
+  );
+  const [enabled, setEnabled] = useState(store.get("map enabled") || true);
 
   useEffect(() => {
-    console.log("GPS", gps);
+    console.log("GPS", { lat, lng });
     const { length } = history;
     if (
-      gps &&
-      (length === 0 ||
-        gps.lat !== history[length - 1][0] ||
-        gps.lng !== history[length - 1][1])
+      length === 0 ||
+      lng !== history[length - 1][0] ||
+      lat !== history[length - 1][1]
     ) {
-      const newHistory = [...history, [gps.lng, gps.lat]];
+      const newHistory = [...history, [lng, lat]];
+      console.log(newHistory);
       setHistory(newHistory);
       store.set("gps history", newHistory);
     }
-  }, [gps, history]);
+  }, [lat, lng, history]);
 
   return (
-    <div className="map-container">
-      <Amap zoom={15} center={center}>
-        <Marker
-          position={center}
-          label={{
-            direction: "bottom",
-          }}
-          draggable
-        />
-      </Amap>
+    <div className={styles.mapContainer}>
+      <Switch
+        className={styles.switch}
+        onChange={setEnabled}
+        checked={enabled}
+      />
+      {enabled && (
+        <Amap zoom={15} center={center}>
+          <Marker
+            position={center}
+            label={{
+              direction: "bottom",
+            }}
+            draggable
+          />
+          <Polyline path={history} />
+        </Amap>
+      )}
     </div>
   );
 }
