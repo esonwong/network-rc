2af73699fee670a31818ea8f11ec3a50eed03702
Eson Wong <itiwll@gmail.com>
2022-03-13 22:09:59 +0800

    Fix: map component

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -6,31 +6,29 @@ import styles from "./Map.module.scss";
 
 config.key = "8faf092bfa96e5b6748ea7e0a2d6ac9c";
 
-export default function Map({
-  editabled = false,
-  statusInfo: { gps = {} } = {},
-}) {
-  const { lat = 39, lng = 116 } = gps;
+export default function Map({ editabled = false, statusInfo: { gps } = {} }) {
+  const [history, setHistory] = useState(store.get("gps history") || []);
+  const [defaultLng, defaultLat] = history.length
+    ? history[history.length - 1]
+    : [117.2, 34.2];
+  const { lat = defaultLat, lng = defaultLng } = gps || {};
   const center = [lng, lat];
-
-  const [history, setHistory] = useState(
-    (store.get("gps history") || [])
-      .filter(({ lat, lng }) => lat !== undefined && lng !== undefined)
-      .slice(0, 1000)
-  );
   const [enabled, setEnabled] = useState(store.get("map enabled") || true);
   const [zoom, setZoom] = useState(store.get("map zoom") || 15);
 
   useEffect(() => {
     console.log("GPS", { lat, lng });
+    console.log("history", history);
     const { length } = history;
     if (
       length === 0 ||
       lng !== history[length - 1][0] ||
       lat !== history[length - 1][1]
     ) {
-      const newHistory = [...history, [lng, lat]];
-      console.log(newHistory);
+      const newHistory = [...history, [lng, lat]]
+        .filter(([lng, lat]) => lat !== undefined && lng !== undefined)
+        .slice(0, 1000);
+      console.log("newHistory", newHistory);
       setHistory(newHistory);
       store.set("gps history", newHistory);
     }
@@ -53,26 +51,27 @@ export default function Map({
             <Button onClick={() => setHistory([])}>清空</Button>
           </Space>
         </div>
-      ) : (
-        <Slider
-          className={styles.zoom}
-          min={2}
-          max={20}
-          value={zoom}
-          onChange={(v) => setZoom(v)}
-        />
-      )}
+      ) : null}
       {enabled && (
-        <Amap zoom={zoom} center={center}>
-          <Marker
-            position={center}
-            label={{
-              direction: "bottom",
-            }}
-            draggable
+        <>
+          <Slider
+            className={styles.zoom}
+            min={2}
+            max={20}
+            value={zoom}
+            onChange={(v) => setZoom(v)}
           />
-          <Polyline path={history} />
-        </Amap>
+          <Amap zoom={zoom} center={center}>
+            <Marker
+              position={center}
+              label={{
+                direction: "bottom",
+              }}
+              draggable
+            />
+            <Polyline path={history} />
+          </Amap>
+        </>
       )}
     </div>
   );
