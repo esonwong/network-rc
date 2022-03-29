8c440d608cb9b80e7d15fba9192ca7f58ec202af
Eson Wong <esonzero@gmail.com>
2022-03-29 15:31:45 +0100

    Feature: 替换 TTS 服务

--- test/tts.test.js
+++ test/tts.test.js
@@ -1,7 +1,8 @@
 const audioPlayer = require("../lib/audioPlayer");
 const speak = require("../lib/tts");
 
-audioPlayer.on("ready", () => {
-  console.log("audioPlayer ready");
-  speak("播报测试");
+audioPlayer.on("ready", async () => {
+  // await speak.tts("test", "/home/pi/.network-rc/tts/test.mp3");
+  // console.log("audioPlayer ready");
+  await speak("语音测试");
 });
